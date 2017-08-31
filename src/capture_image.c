bcdd0e4cdb68fda78e440e7e3e997b3f70b29d60
cmruderman <coreymruderman@gmail.com>
2017-08-31 16:10:51 -0400

    Verilog addition

--- src/capture_image.c
+++ src/capture_image.c
@@ -46,21 +46,15 @@
 	/*Global Vars*/
 	int bW=0; //0 if not b&w or has been already inverted backward 	1 if inverted forward
 	int bwReset=0;   //make sure screen isnt b&w
-	int screen_index=0; 
-	int horiz=0;
+	int x=0; //keep track of coordinates...x goes from 0 to 320
+	int y=0; //keep track of coordinates...y goes from 0 to 219
 	int sdram_index=0;
-	int vert=0;
-	int x=0;
-	int y=0;
 	int sum=0;  //keep sum of color of pixels
 	int average=0;  //sum of color of pixels/# of pixels
-	int RLEsegments=0;
-	float compRatio = 0;
+	int comp = 0; // keep track of bytes compressed
 	int decomp = 0;
 	
-	unsigned char pixelBytes[(320*240)/8];
-	char screen[320*240];
-	unsigned char compressed[(320*240)*3]; //3 bytes per pixel
+	unsigned char pixelBytes[(320*240)/8]; //store one bit per pixel in groups of 8 bits
 	
 int main(void)
 {
@@ -116,7 +110,7 @@ void blackScreen(){
 	*(Video_In_DMA_ptr + 3) = 0x0;
 	for (y = 0; y < 240; y++) {
 		for (x = 0; x < 320; x++) {
-			*(Video_Mem_ptr + (y << 9) + x)=0x0;
+			*(Video_Mem_ptr + (y << 9) + x)=0x0; //make every pixel black
 		}
 	}	
 }
@@ -166,76 +160,72 @@ void blackAndWhite(){
 	sum=0; //make the sum 0 so we can call blackandWhite again
 }
 
-void pixelInBytes(){
+void pixelInBytes(){ //count black and white pixels...store as one-bit-per-pixel
 	int ct=0;
 	unsigned int arrayIndex=0;
 	unsigned char data=0;
 	for (y=0;y<240; y++) {
 		for (x=0;x<320; x++) {
-			data = (*(Video_Mem_ptr + (y << 9) + x)==0x0)? (data>>1) : ((data>>1)| 0x80);
-			if(ct<7)
+			data = (*(Video_Mem_ptr + (y << 9) + x)==0x0)? (data>>1) : ((data>>1)| 0x80); //if pixel is black, store as 0
+			if(ct<7) //if we havent read in 8 bits keep going
 				ct++;
-			else{
+			else{ //on the 8th bit
 				ct=0;
-				pixelBytes[arrayIndex]= data;
-				arrayIndex++;
+				pixelBytes[arrayIndex]= data; //store the byte into the array
+				arrayIndex++; //increment the index
 				data=0;
 			}
 		}
 	}	
 }
 
-void outOfSDRAM(){
+void outOfSDRAM(){ //takes out of sdram and puts on screen
 	int pixel=0;
 	for (y=0;y<240; y++) {
 		for (x=0;x<320; x++) {
 			pixel = *(SDRAM_ptr+x+320*y);
-			*(Video_Mem_ptr + (y << 9) + x-8) = (pixel==1)? 0xFFFF : 0x0000;
+			*(Video_Mem_ptr + (y << 9) + (x-8)) = (pixel==1)? 0xFFFF : 0x0000; //if value is 1, make pixel white, other...make black
 		}
 	}
-	printf("Compression Ratio %f.\n", decomp/compRatio);
-	decomp = 0;
-	compRatio = 0;
+	float comp_in_bytes=(float)comp/8; //need compressed count in bytes
+	printf("9600 bytes decoded.\n");
+	printf("%.2f bytes encoded.\n", comp_in_bytes);
+	float ratio = (float)9600/comp_in_bytes; // compression ratio
+	printf("Compression Ratio %.4f\n", ratio);
+	comp = 0;
 }
 
 void decompress(int RLE_Return){
-	char bitVal = (RLE_Return & 0x00800000)>>23;
-	int quantity=(RLE_Return&0x007FFFFF);
-	compRatio = compRatio+24;
-	decomp=decomp+quantity;
+	char bitVal = (RLE_Return & 0x00800000)>>23; //Only keep top 24th bit
+	int quantity=(RLE_Return&0x007FFFFF); //only keep 23 bottom bits
+	comp+=24; //increase count by 24 because we just read in 24 bits
 	int i;
 	for(i=0;i<quantity;i++) {
-		*(SDRAM_ptr +sdram_index)=bitVal;
+		*(SDRAM_ptr +sdram_index)=bitVal; //assign bitVal to the sdram equal to quantity # of times
 		sdram_index++;
 	}
 }
 
 void DecompressCompress(){
 	long int compressCt=0;
-	long int enc=0;
-	screen_index=0; 
-	horiz=0;
-	vert=0;
-	blackScreen();
+	blackScreen(); //make screen all black
 	printf("Encoding\n");
-	while(compressCt<=(9600)){
-			alt_write_byte(ALT_FPGA_BRIDGE_LWH2F_OFST+FIFO_IN_WRITE_REQ_PIO_BASE,1);
-			alt_write_byte(ALT_FPGA_BRIDGE_LWH2F_OFST+ODATA_PIO_BASE, pixelBytes[compressCt]);
-			alt_write_byte(ALT_FPGA_BRIDGE_LWH2F_OFST+FIFO_IN_WRITE_REQ_PIO_BASE,0);
+	while(compressCt<=(9600)){ //make sure count is less than 9600...screen dimension in bytes
+			alt_write_byte(ALT_FPGA_BRIDGE_LWH2F_OFST+FIFO_IN_WRITE_REQ_PIO_BASE,1); //asserted to write bitstream to FIFO in buffer
+			alt_write_byte(ALT_FPGA_BRIDGE_LWH2F_OFST+ODATA_PIO_BASE, pixelBytes[compressCt]); //write a byte into the RLE
+			alt_write_byte(ALT_FPGA_BRIDGE_LWH2F_OFST+FIFO_IN_WRITE_REQ_PIO_BASE,0); //deasserted after writing
 			compressCt++;
 		if(alt_read_byte(RESULT_READY_PIO_BASE+ALT_FPGA_BRIDGE_LWH2F_OFST)==0){
-			alt_write_byte(ALT_FPGA_BRIDGE_LWH2F_OFST+FIFO_OUT_READ_REQ_PIO_BASE,1);
-			decompress(alt_read_word(ALT_FPGA_BRIDGE_LWH2F_OFST+IDATA_PIO_BASE));
-			enc+=3;
-			alt_write_byte(ALT_FPGA_BRIDGE_LWH2F_OFST+FIFO_OUT_READ_REQ_PIO_BASE,0);				
+			alt_write_byte(ALT_FPGA_BRIDGE_LWH2F_OFST+FIFO_OUT_READ_REQ_PIO_BASE,1); //enable write signal
+			decompress(alt_read_word(ALT_FPGA_BRIDGE_LWH2F_OFST+IDATA_PIO_BASE)); //take the 3 bytes out of the RLE and decompress
+			alt_write_byte(ALT_FPGA_BRIDGE_LWH2F_OFST+FIFO_OUT_READ_REQ_PIO_BASE,0);	 //disable write signal			
 		}
 	}
-	alt_write_byte(ALT_FPGA_BRIDGE_LWH2F_OFST+RLE_FLUSH_PIO_BASE, 1);
+	alt_write_byte(ALT_FPGA_BRIDGE_LWH2F_OFST+RLE_FLUSH_PIO_BASE, 1); //buffer is flushed after end of bit stream
 	alt_write_byte(ALT_FPGA_BRIDGE_LWH2F_OFST+RLE_FLUSH_PIO_BASE, 0);
-	alt_write_byte(FIFO_OUT_READ_REQ_PIO_BASE+ALT_FPGA_BRIDGE_LWH2F_OFST,1);
+	alt_write_byte(FIFO_OUT_READ_REQ_PIO_BASE+ALT_FPGA_BRIDGE_LWH2F_OFST,1); 
 	decompress(alt_read_word(IDATA_PIO_BASE+ALT_FPGA_BRIDGE_LWH2F_OFST));
 	alt_write_byte(FIFO_OUT_READ_REQ_PIO_BASE+ALT_FPGA_BRIDGE_LWH2F_OFST,0);
-	printf("%ld RLE segments encoded.\n", enc/3);
-	sdram_index=0;
+	sdram_index=0; //reset index for SDRAM
 	outOfSDRAM();
 }
